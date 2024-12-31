5fa1a9c50eeb137745ce85843f3ad699b4def9d4
media-sec-lab <85596727+media-sec-lab@users.noreply.github.com>
2024-12-31 11:50:21 +0800

    Delete Steganographic code directory

--- Steganographic code/stc/STC matlab implementation/calc_relative_payload.m
+++
@@ -1,8 +0,0 @@
-function [ alpha ] = calc_relative_payload( code )
-%CALC_CODE_RATE calculates relative payload (1-rate) of a given code.
-%
-% Tomas Filler (tomas.filler@binghamton.edu)
-% http://dde.binghamton.edu/filler
-
-alpha = sum(code.shift)/code.n;
-end
